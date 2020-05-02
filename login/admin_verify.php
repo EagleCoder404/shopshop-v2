4da901cb64671715d8869e46d5417a87d94dd146
code_10 <manojkumar333619@gmail.com>
2020-05-02 21:17:36 +0530

    Update admin_verify.php

--- login/admin_verify.php
+++ login/admin_verify.php
@@ -1,3 +1,29 @@
 <?php
-  echo "hello admin";
+        $category=Array();
+        $con = getCon();
+        $res = $con->query("select * from categories");
+        while($ele = $res->fetch_assoc())
+            $category[]=$ele;
+        
+        $category_id = $_POST['cat_id'];
+        $category_name = $_POST['cat_name'];
+        $con->query("insert into categories values('$category_id','$category_name')");
+        
+        
 ?>
+    <div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">categories</a>
+      </h4>
+    </div>
+    <div class="text-center">
+    <div id="collapse1" class="panel-collapse collapse in">
+      <? foreach($category as $cat) { ?>
+            <p><?=$cat['cat_id']."=>".$cat['cat_name']?></p>
+            <? } ?>
+    </div>
+    </div>
+  </div>
+  </div>
