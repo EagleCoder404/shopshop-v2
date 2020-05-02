6558cb808909b2a24d1ba26f5b4da049ff162471
code_10 <manojkumar333619@gmail.com>
2020-05-03 00:08:05 +0530

    Update admin_verify.php

--- login/admin_verify.php
+++ login/admin_verify.php
@@ -1,31 +1,36 @@
 <?php
         session_start();
         include '../libraries/chocolates.php';
-        $con = getCon();
+        
         /*$category=Array();
         $con = getCon();
         $res = $con->query("select * from categories");
         while($ele = $res->fetch_assoc())
             $category[]=$ele;*/
         
-        $cat_id = $_POST['cat_id'];
-        $cat_name = $_POST['cat_name'];
-        //$con->query("insert into categories values('$cat_id','$cat_name')");
         
-        if(($con->query("insert into categories(cat_id,cat_name) values('$cat_id','$cat_name')"))===True){
+        //$con->query("insert into categories values('$cat_id','$cat_name')");
+        if (!empty($_POST['categories_submit'])) {
+                $cat_id = $_POST['cat_id'];
+        $cat_name = $_POST['cat_name'];
+                $con = getCon();
+              if(($con->query("insert into categories(cat_id,cat_name) values('$cat_id','$cat_name')"))===True){
                 //echo "YES";
                 header("Location:admin_display.php");
                 die();
         }
         else
-                echo $con->error;   
+                echo $con->error;  
+        }
+           
 
         /*$sub_category=Array();
         $res = $con->query("select * from sub_categories");
         while($ele = $res->fetch_assoc())
             $sub_category[]=$ele;*/
-        
-        $sub_cat_id = $_POST['sub_cat_id'];
+        if (!empty($_POST['sub_categories_submit'])) {
+                $con = getCon();
+                $sub_cat_id = $_POST['sub_cat_id'];
         $sub_cat_name = $_POST['sub_cat_name'];
         $s_cat_id = $_POST['s_cat_id'];
         //$con->query("insert into categories values('$sub_cat_id','$sub_cat_name')");
@@ -37,6 +42,8 @@
         }
         else
                 echo $con->error;
+        }
+        
         
 ?>
 <a href="admin_display.php">something is wrong let me go back</a>
