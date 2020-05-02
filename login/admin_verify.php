487b64743081ca45da0473eab686313c07d3b717
code_10 <manojkumar333619@gmail.com>
2020-05-02 21:49:13 +0530

    Update admin_verify.php

--- login/admin_verify.php
+++ login/admin_verify.php
@@ -1,6 +1,6 @@
 <?php
         session_start();
-    include '../libraries/chocolates.php';
+        include '../libraries/chocolates.php';
         $category=Array();
         $con = getCon();
         $res = $con->query("select * from categories");
@@ -13,22 +13,68 @@
         
         
 ?>
-    <div class="panel-group" id="accordion">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">categories</a>
-      </h4>
+
+<style>
+    .bs-example{
+        margin: 20px;
+    }
+    .accordion .fa{
+        margin-right: 0.5rem;
+    }
+</style>
+<script>
+    $(document).ready(function(){
+        // Add minus icon for collapse element which is open by default
+        $(".collapse.show").each(function(){
+        	$(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+        });
+        
+        // Toggle plus minus icon on show hide of collapse element
+        $(".collapse").on('show.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+        }).on('hide.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+        });
+    });
+</script>
+</head>
+<body>
+<div class="bs-example">
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                    <button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"><i class="fa fa-plus"></i> What is HTML?</button>									
+                </h2>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                <div class="card-body">
+                     <? foreach($category as $cat) { ?>
+                        <p><?=$cat['cat_id']."=>".$cat['cat_name']?></p>
+                        <? } ?>
+                </div>
+            </div>
+        </div>
+        <!--<div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo"><i class="fa fa-plus"></i> What is Bootstrap?</button>
+                </h2>
+            </div>
+            <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                    <p>Bootstrap is a sleek, intuitive, and powerful front-end framework for faster and easier web development. It is a collection of CSS and HTML conventions. <a href="https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>-->
+        
     </div>
-    <div class="text-center">
-    <div id="collapse1" class="panel-collapse collapse in">
-      <? foreach($category as $cat) { ?>
-            <p><?=$cat['cat_id']."=>".$cat['cat_name']?></p>
-            <? } ?>
-    </div>
-    </div>
-  </div>
-  </div>
+</div>
+</body>
+</html>                            
+    
+     
+   
 
 
 <a href='admin_enter.php'>click me to go back</a>
