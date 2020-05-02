e74a3feec9f8c7a41d51b59c0b1edd3708262619
code_10 <manojkumar333619@gmail.com>
2020-05-02 23:21:48 +0530

    Update admin_verify.php

--- login/admin_verify.php
+++ login/admin_verify.php
@@ -27,9 +27,10 @@
         
         $sub_cat_id = $_POST['sub_cat_id'];
         $sub_cat_name = $_POST['sub_cat_name'];
+        $cat_id = $_POST['s_cat_id'];
         //$con->query("insert into categories values('$sub_cat_id','$sub_cat_name')");
         
-        if(($con->query("insert into sub_categories(sub_cat_id,sub_cat_name,cat_id) values('$sub_cat_id','$sub_cat_name','$cat_id')"))===True){
+        if(($con->query("insert into sub_categories(sub_cat_id,sub_cat_name,cat_id) values('$sub_cat_id','$sub_cat_name','$s_cat_id')"))===True){
                 //echo "YES";
                 header("Location:admin_display.php");
                 die();
