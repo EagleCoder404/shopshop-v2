50734ba1d197de5e2b164322952dcad0ff9fa7bf
code_10 <manojkumar333619@gmail.com>
2020-05-03 12:06:11 +0530

    Update register_details.php

--- register/register_details.php
+++ register/register_details.php
@@ -2,7 +2,7 @@
 
   //including for calling useful functions 
   include '../libraries/chocolates.php';
-
+  if(isset($_POST['register_user'])){
   //to get connection from db
   $con=getCon();
 
@@ -25,5 +25,32 @@
   }
   else
     echo $con->error;
+  }
 
+
+  if(isset($_POST['register_seller'])){
+  //to get connection from db
+  $con=getCon();
+
+  //getting username and password from regsiter.php
+  $s_u = $_POST['seller_user_name'];
+  $s_e = $_POST['seller_email'];
+  //$n = $_POST['name'];
+  $s_p = $_POST['seller_password'];
+  /*$city = $_POST['city'];
+  $state = $_POST['state'];
+  $country = $_POST['country'];*/
+  $s_p = password_hash($p,PASSWORD_DEFAULT);
+
+
+  //checking if query is valid if yes then yes else respective error will be displayed
+  if(($con->query("insert into seller(seller_user_name,seller_email,password) values('$s_u','$s_e','$s_p');"))===True){
+     //echo "YES";
+      header("Location:../login/seller_login.php");
+      die();
+  }
+  else
+    echo $con->error;
+  }
+  
 ?>
