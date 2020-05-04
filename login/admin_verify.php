8f41e8b961c070c9ce5d50073646e4c77539c34a
manikr17 <63992542+manikr17@users.noreply.github.com>
2020-05-04 20:40:20 +0530

    added products

--- login/admin_verify.php
+++ login/admin_verify.php
@@ -32,10 +32,10 @@
                 $con = getCon();
                 $sub_cat_id = $_POST['sub_cat_id'];
         $sub_cat_name = $_POST['sub_cat_name'];
-        $s_cat_id = $_POST['s_cat_id'];
+        $cat_id = $_POST['cat_id'];
         //$con->query("insert into categories values('$sub_cat_id','$sub_cat_name')");
         
-        if(($con->query("insert into sub_categories(sub_cat_id,sub_cat_name,cat_id) values('".mysqli_real_escape_string($con,$sub_cat_id)."','".mysqli_real_escape_string($con,$sub_cat_name)."','".mysqli_real_escape_string($con,$s_cat_id)."')"))===True){
+        if(($con->query("insert into sub_categories(sub_cat_id,sub_cat_name,cat_id) values('".mysqli_real_escape_string($con,$sub_cat_id)."','".mysqli_real_escape_string($con,$sub_cat_name)."','".mysqli_real_escape_string($con,$cat_id)."')"))===True){
                 //echo "YES";
                 header("Location:admin_display.php");
                 die();
@@ -43,6 +43,30 @@
         else
                 echo $con->error;
         }
+
+
+       
+        
+
+        if (isset($_POST['products'])) {
+                $con = getCon();
+                $product_id = $_POST['product_id'];
+                $product_name = $_POST['product_name'];
+                $sub_cat_id = $_POST['sub_cat_id'];
+                $product_brand = $_POST['product_brand'];
+                $product_description = $_POST['product_description'];
+                $product_rating = $_POST['product_rating'];
+        //$con->query("insert into product values('$product_id','$product_id', and so on)");
+        
+        if(($con->query("insert into products($product_id,product_name,sub_cat_id,product_brand,product_description,rating) values('".mysqli_real_escape_string($con,$product_id)."','".mysqli_real_escape_string($con,$product_name)."','".mysqli_real_escape_string($con,$sub_cat_id)."','".mysqli_real_escape_string($con,$product_brand)."','".mysqli_real_escape_string($con,$product_description)."','".mysqli_real_escape_string($con,$product_rating)."')"))===True){
+                //echo "YES";
+                header("Location:admin_display.php");
+                die();
+        }
+        else
+                echo $con->error;
+        }
+
         
         
 ?>
