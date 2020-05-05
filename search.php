ea53c39a428815bc906919ca881dfcc4a8a6da84
manikr17 <63992542+manikr17@users.noreply.github.com>
2020-05-05 15:02:57 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,9 +1,8 @@
 <?php
         session_start();
         
-      if (isset($_POST['search_results'])) {
                 $con = getCon();
-                $search_prod = $_POST['search_product'];
+                $search_prod = $_GET['search_product'];
                 
                 $res = $con->query("select * from products where product_description like 'test'");
 
@@ -39,12 +38,6 @@
                   $res1 = $con->query("select min(price) from unique_product where product_id= '$prod_id[$i]'");
                   while($ele1 = $res1->fetch_assoc())
                   $min_price[]=$ele1["min(price)"];
-                }
-        }
-        else
-                echo $con->error;
-        }
-      
 ?>
 
 
