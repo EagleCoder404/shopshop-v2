87eb0d222f8e17711ea62cde2064a62eff5d5a04
code_10 <manojkumar333619@gmail.com>
2020-05-05 18:53:01 +0530

    Create wishlist.php

---
+++ product/wishlist.php
@@ -0,0 +1,20 @@
+<?php
+  
+  session_start();
+  include '../libraries/chocolates.php';
+  
+  $product_id=$_GET['product_id'];
+  $user=$_SESSION['user_name'];
+  
+  if(isset($_SESSION['user_name']))
+  {
+      if($con->query("insert into wishlist(user_name,product_id) values('$user','$product_id')")===True)
+      {
+        header(Location:'product_description.php');
+        die();
+      }
+      else
+        echo $con->error;
+  }
+    
+?>
