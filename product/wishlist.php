9ea6151eea3aacc2ea1a71bff460122e19d7988d
code_10 <manojkumar333619@gmail.com>
2020-05-07 14:21:56 +0530

    Update wishlist.php

--- product/wishlist.php
+++ product/wishlist.php
@@ -6,17 +6,18 @@
 
   $product_id=$_GET['product_id'];
   $user=$_SESSION['user_name'];
+  $product_name=$_GET['product_name'];
   
   if(isset($_SESSION['user_name']))
   {
       if(($con->query("insert into wishlist(user_name,product_id) values('$user','$product_id');"))===True)
       {
-        header("Location:product_description.php");
+        header("Location:product_description.php?product_id=<?=$product_id?>&&product_name=<?=$product_name?>");
         die();
       }
       else
       {
-        header("Location:product_description.php");
+        header("Location:product_description.php?product_id=<?=$product_id?>&&product_name=<?=$product_name?>");
         die();
         //header("Location:product_description.php");
         //die();
@@ -24,7 +25,7 @@
   }
 else
 {
-  header("Location:product_description.php");
+  header("Location:product_description.php?product_id=<?=$product_id?>&&product_name=<?=$product_name?>");
         die();
 }
     
